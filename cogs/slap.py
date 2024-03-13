9a57c4ffe4fa09eff059110fcf44ad84c6f49946
Trix00000 <trixindialimited00@gmail.com>
2024-03-13 05:32:11 +0530

    slap command

---
+++ cogs/slap.py
@@ -0,0 +1,54 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+import praw
+import random
+
+
+class SlapCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="slap")
+    async def slap(self, ctx: commands.Context, user: discord.Member):
+        
+        if ctx.author == user:
+
+            dumb_titles_list = []
+
+            with open("files/slap_dumb.txt", "r") as f:
+                for line in f:
+                    dumb_titles_list.append(line.strip())
+            embed = discord.Embed(title=random.choice(dumb_titles_list), color=discord.Color.random())
+            await ctx.send(embed=embed)
+
+
+
+        else:
+            titles_list = []
+            gif_list = []
+
+            with open("files/slap_text.txt", "r") as f:
+                for line in f:
+                    titles_list.append(line.strip())
+
+            with open("files/slap_gif.txt", "r") as f:
+                for line in f:
+                    gif_list.append(line.strip())
+
+            title = random.choice(titles_list)
+            gif = random.choice(gif_list)
+            formated_title = title.replace("[User]", user.mention).replace("[Author]", ctx.author.mention)
+            embed = discord.Embed(title="", description=f"**{formated_title}**", color=discord.Color.random())
+            embed.set_image(url=gif)
+            await ctx.reply(embed=embed)
+        
+        
+
+        
+            
+            
+            
+async def setup(bot):
+    await bot.add_cog(SlapCog(bot))   
+    
